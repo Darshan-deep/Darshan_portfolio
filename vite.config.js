f50f9df8b7b30940586eceb494b6008da3c5c11a
Darshan-deep <thapadeep2003@gmail.com>
2026-02-05 11:02:41 +0545

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,93 @@
+// import {
+//   vitePlugin as remix,
+//   cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+// } from '@remix-run/dev';
+// import { defineConfig } from 'vite';
+// import jsconfigPaths from 'vite-jsconfig-paths';
+// import mdx from '@mdx-js/rollup';
+// import remarkFrontmatter from 'remark-frontmatter';
+// import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+// import rehypeImgSize from 'rehype-img-size';
+// import rehypeSlug from 'rehype-slug';
+// import rehypePrism from '@mapbox/rehype-prism';
+
+// export default defineConfig({
+//   assetsInclude: ['**/*.glb', '**/*.hdr', '**/*.glsl'],
+//   build: {
+//     assetsInlineLimit: 1024,
+//   },
+//   server: {
+//     port: 3000,
+//   },
+//   plugins: [
+//     mdx({
+//       rehypePlugins: [[rehypeImgSize, { dir: 'public' }], rehypeSlug, rehypePrism],
+//       remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+//       providerImportSource: '@mdx-js/react',
+//     }),
+//     remixCloudflareDevProxy(),
+//     remix({
+//       routes(defineRoutes) {
+//         return defineRoutes(route => {
+//           route('/', 'routes/home/route.js', { index: true });
+//         });
+//       },
+//     }),
+//     jsconfigPaths(),
+//   ],
+// });
+
+
+
 import {
   vitePlugin as remix,
-  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
-} from '@remix-run/dev';
-import { defineConfig } from 'vite';
-import jsconfigPaths from 'vite-jsconfig-paths';
-import mdx from '@mdx-js/rollup';
-import remarkFrontmatter from 'remark-frontmatter';
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
-import rehypeImgSize from 'rehype-img-size';
-import rehypeSlug from 'rehype-slug';
-import rehypePrism from '@mapbox/rehype-prism';
+} from "@remix-run/dev";
+import { vercelPreset } from "@vercel/remix/vite";
+import { defineConfig } from "vite";
+import jsconfigPaths from "vite-jsconfig-paths";
+
+import mdx from "@mdx-js/rollup";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import rehypeImgSize from "rehype-img-size";
+import rehypeSlug from "rehype-slug";
+import rehypePrism from "@mapbox/rehype-prism";
 
 export default defineConfig({
-  assetsInclude: ['**/*.glb', '**/*.hdr', '**/*.glsl'],
+  assetsInclude: ["**/*.glb", "**/*.hdr", "**/*.glsl"],
+
   build: {
     assetsInlineLimit: 1024,
   },
+
   server: {
-    port: 7777,
+    port: 3000,
   },
+
   plugins: [
     mdx({
-      rehypePlugins: [[rehypeImgSize, { dir: 'public' }], rehypeSlug, rehypePrism],
-      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
-      providerImportSource: '@mdx-js/react',
+      rehypePlugins: [
+        [rehypeImgSize, { dir: "public" }],
+        rehypeSlug,
+        rehypePrism,
+      ],
+      remarkPlugins: [
+        remarkFrontmatter,
+        remarkMdxFrontmatter,
+      ],
+      providerImportSource: "@mdx-js/react",
     }),
-    remixCloudflareDevProxy(),
+
     remix({
+      presets: [vercelPreset()],
+
       routes(defineRoutes) {
-        return defineRoutes(route => {
-          route('/', 'routes/home/route.js', { index: true });
+        return defineRoutes((route) => {
+          route("/", "routes/home/route.js", { index: true });
         });
       },
     }),
+
     jsconfigPaths(),
   ],
-});
+});``
